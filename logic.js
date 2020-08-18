41ab35ad3d5dc82ed6f337ff8d374b2d9bdf99d7
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2020-08-18 12:28:22 -0400

    Proper Money formatting
    Table is now sortable with proper money formatting

--- logic.js
+++ logic.js
@@ -3,6 +3,11 @@ let MARKET_URL="/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10
 
 let marketList = BASE_URL + MARKET_URL;
 
+const formatter = new Intl.NumberFormat('en-US',{
+  style: 'currency',
+  currency: 'USD',
+  maximumSignificantDigits: 19
+});
 
 
 let coins = [marketList]; //will add more urls to get 24h volume
@@ -10,7 +15,7 @@ let coins = [marketList]; //will add more urls to get 24h volume
 Promise.all(coins.map(urls => {
   return fetch(urls).then(res =>
     res.json())
-}))
+  }))
 .then(data => {
   console.log(data);
 
@@ -28,64 +33,66 @@ Promise.all(coins.map(urls => {
       let marketCap = data[0][i].market_cap;
       let low24 = data[0][i].low_24h;
       let high24 = data[0][i].high_24h;
-      let chartData = data[0][i].sparkline_in_7d.price;
+
 
       let hourGainLoss = hour >= 0  ? addClass = "gain" : addClass = "loss";
       let dayGainLoss = day >= 0  ? addClass = "gain" : addClass = "loss";
       let sevenDayGainLoss = sevenDay >= 0  ? addClass = "gain" : addClass = "loss";
 
-      let cell1 = '<td/ class="rank">' + rank;
 
+      let cell1 = '<td/ class="rank">' + rank;
       let cell2 = '<td/ class="name ticker text-left">' + '<img alt="' + name + ' logo" class="logo" src="' + logo + '">' + ' ' + name;
       let cell3 = '<td/ class="ticker">' + ticker;
-      let cell4 = '<td/ class="price text-right">' + "$" + price;
+      let cell4 = '<td/ class="price text-right">' + formatter.format(price) + '<p/ class="text-right highLow high">' + "24h high " + formatter.format(high24) + '<p/ class="text-right highLow low">' + "24h low " + formatter.format(low24);
       let cell5 = '<td/ class="hour '+ hourGainLoss + '">' + parseFloat(hour || 0).toFixed(1) + "%";
       let cell6 = '<td/ class="day '+ dayGainLoss + '">' + parseFloat(day || 0).toFixed(1) + "%";
       let cell7 = '<td/ class="sevenDay '+ sevenDayGainLoss + '">' + parseFloat(sevenDay || 0).toFixed(1) + "%";
-      let cell8 = '<td/ class="volume text-right">' + "$" + volume;
-      let cell9 = '<td/ class="marketCap text-right">' + "$" + marketCap;
+      let cell8 = '<td/ class="volume text-right">' + formatter.format(volume);
+      let cell9 = '<td/ class="marketCap text-right">'  + formatter.format(marketCap);
 
       let row = '<tr/>' + cell1 + cell2 + cell3 + cell4 + cell5 + cell6 + cell7 + cell8 + cell9;
 
       $("tbody").append(row);
     };
+    //heavily helped by a video to reach this function
+    function sortTable(table, column, asc = true){
+      const dirModifier = asc ? 1 : -1;
+      const tBody = table.tBodies[0];
+      const rows = Array.from(tBody.querySelectorAll("tr"));
+        //sort each rows
+      const sortedRows = rows.sort((a, b) => {
+      if (column !== 1){
+        const aColPrice = parseFloat((a.querySelector(`td:nth-child(${column + 1})`)).textContent.trim().replace("$",'').replace(/,/g,""));
+        const bColPrice = parseFloat((b.querySelector(`td:nth-child(${column + 1})`)).textContent.trim().replace("$",'').replace(/,/g,""));
+          return aColPrice > bColPrice ? (1 * dirModifier) : (-1 * dirModifier);
+      }  else {
+          const aColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+          const bColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+            return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+      }});
+      //remove all existing trs from the table
+      while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+      }
+      //readd newly sortable rows
+      tBody.append(...sortedRows);
+      //remember which columm is sorting
+      table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+      table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
+      table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+    };
 
-      function sortTable(table, column, asc = true){
-        const dirModifier = asc ? 1 : -1;
-        const tBody = table.tBodies[0];
-        const rows = Array.from(tBody.querySelectorAll("tr"));
-          //sort each rows
-        const sortedRows = rows.sort((a, b) => {
-        if (column !== 1){
-          const aColPrice = parseFloat(a.querySelector(`td:nth-child(${column + 1})`).textContent.trim().replace('$',""));
-          const bColPrice = parseFloat(b.querySelector(`td:nth-child(${column + 1})`).textContent.trim().replace('$',""));
-            return aColPrice > bColPrice ? (1 * dirModifier) : (-1 * dirModifier);
-        }  else {
-            const aColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-            const bColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-              return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
-        }});
-        //remove all existing trs from the table
-        while (tBody.firstChild) {
-          tBody.removeChild(tBody.firstChild);
-        }
-        //readd newly sortable rows
-        tBody.append(...sortedRows);
-        //remember which columm is sorting
-        table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
-        table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
-        table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
-      };
+    document.querySelectorAll(".table-sortable .clickable").forEach(headerCell => {
+      headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAsc = headerCell.classList.contains("th-sort-asc");
 
-      document.querySelectorAll(".table-sortable .clickable").forEach(headerCell => {
-        headerCell.addEventListener("click", () => {
-          const tableElement = headerCell.parentElement.parentElement.parentElement;
-          const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
-          const currentIsAsc = headerCell.classList.contains("th-sort-asc");
 
-          sortTable(tableElement, headerIndex, !currentIsAsc);
-        });
+        sortTable(tableElement, headerIndex, !currentIsAsc);
       });
+    });
+
 })
 .catch (err =>{
   console.log(err);
